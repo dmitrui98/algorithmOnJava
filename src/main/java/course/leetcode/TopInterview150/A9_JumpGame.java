2628def4b307d2a6f98d2721aaaa0e42542d27d5
d.golikov <dmitrui98@mail.ru>
2024-07-17 23:47:26 +0900

    leetcode JumpGame task solved

--- src/main/java/course/leetcode/TopInterview150/A9_JumpGame.java
+++ src/main/java/course/leetcode/TopInterview150/A9_JumpGame.java
@@ -17,7 +17,6 @@ public class A9_JumpGame {
 
     private void go() {
         int[] a;
-        /*
         // Jump 1 step from index 0 to 1, then 3 steps to the last index.
         a = new int[]{2,3,1,1,4}; // true
         ArrayUtil.printArray(a);
@@ -28,46 +27,48 @@ public class A9_JumpGame {
         ArrayUtil.printArray(a);
         System.out.println("Can jump: " + canJump(a));
 
-
-        a = new int[]{1,1,1,0};
+        a = new int[]{1,1,1,0}; // true
         ArrayUtil.printArray(a);
         System.out.println("Can jump: " + canJump(a));
 
-        a = new int[]{1,1,2,2,0,1,1};
+        a = new int[]{1,1,2,2,0,1,1}; // true
         ArrayUtil.printArray(a);
         System.out.println("Can jump: " + canJump(a));
 
-         */
 
-        a = new int[]{7,2,3,2,6,0,0,1,0,1,0};
+        a = new int[]{7,2,3,2,6,0,0,1,0,1,0}; // true
         ArrayUtil.printArray(a);
         System.out.println("Can jump: " + canJump(a));
 
-//        a = new int[]{5,9,3,2,1,0,2,3,3,1,0,0};
-//        ArrayUtil.printArray(a);
-//        System.out.println("Can jump: " + canJump(a));
+        a = new int[]{5,9,3,2,1,0,2,3,3,1,0,0}; // true
+        ArrayUtil.printArray(a);
+        System.out.println("Can jump: " + canJump(a));
     }
 
     public boolean canJump(int[] nums) {
         int i = 0;
         while (i < nums.length) {
-            int imax = i;
-            for (int j = i + 1; j < nums.length && j <= nums[i] + i; j++) {
-                if (nums[j] >= nums[imax]) {
-                    imax = j;
+            int maxSteps = nums[i];
+            int j = i + 1;
+            int step = 1;
+            int path = nums[i];
+            int jumpIndex = i;
+            while (step <= maxSteps && j < nums.length) {
+                int jump = nums[j] + step;
+                if (path < jump) {
+                    path = jump;
+                    jumpIndex = j;
                 }
+                j++;
+                step++;
             }
-            if (nums[imax] + imax >= nums.length - 1) {
+            if (path + i >= nums.length - 1) {
                 return true;
             }
-            if (i == imax) {
-                if (nums[i] == 0) {
-                    return false;
-                }
-                i += nums[i];
-            } else {
-                i = imax;
+            if (jumpIndex == i) {
+                return false;
             }
+            i = jumpIndex;
         }
         return false;
     }
